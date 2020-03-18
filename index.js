0a182f5cff54190db38cc405266c5c0c091ee6ce
Diletta Cala Impirotta <diletta.impirotta@huumun.com>
2020-03-18 11:13:55 +0100

    View - create elements

--- index.js
+++ index.js
@@ -38,7 +38,56 @@ class Model {
 }
 
 class View {
-    constructor() {}
+    constructor() {
+        this.app = this.getElement('#app');
+
+        this.title = this.createElement('h1');
+        this.title.textContent = 'Todos List';
+
+        this.form = this.createElement('form');
+
+        this.input = this.createElement('input');
+        this.input.type = 'text';
+        this.input.placeholder = 'Add todo';
+        this.input.name = 'todo';
+
+        this.submitButton = this.createElement('button');
+        this.submitButton.textContent = 'Add';
+
+        this.todoList = this.createElement('ul', 'todo-list');
+
+        this.form.append(this.input, this.submitButton);
+
+        this.app.append(this.title, this.form, this.todoList);
+    }
+
+    get _todoText() {
+        return this.input.value;
+    }
+
+    _resetInput() {
+        this.input.value = '';
+    }
+
+    createElement(tag, className) {
+        const element = document.createElement(tag);
+        if (className) {
+            element.classList.add(className);
+        }
+
+        return element;
+    }
+
+    getElement(selector) {
+        const element = document.querySelector(selector);
+
+        return element;
+    }
+
+    displayTodos(todos) {
+        // TODO: update view
+    }
+
 }
 
 class Controller {
