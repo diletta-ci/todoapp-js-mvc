d1ae4c394b4ab2a20705060d9f6fd75225fe51c3
Diletta Cala Impirotta <diletta.impirotta@huumun.com>
2020-03-18 10:59:45 +0100

    Model methods

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 class Model {
-    constructor() {}
+    constructor() {
+        this.todos = [
+            {
+                id: 1,
+                text: 'Plant the tulipanes',
+                complete: false,
+            },
+            {
+                id: 2,
+                text: 'Prepare the dinner',
+                complete: false,
+            },
+        ];
+    }
+
+    addTodo(todoText) {
+        const todo = {
+            id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+            text: todoText,
+            complete: false,
+        };
+
+        this.todos.push(todo);
+    }
+
+    editTodo(id, newText) {
+        this.todos = this.todos.map(todo =>
+            todo.id === id ? { id: todo.id, text: newText, complete: todo.complete } : todo
+        );
+    }
+
+    toggleTodo(id) {
+        this.todos = this.todos.map(todo =>
+            todo.id === id ? { id: todo.id, text: todo.text, complete: !todo.complete } : todo
+        );
+    }
 }
 
 class View {
@@ -8,8 +43,8 @@ class View {
 
 class Controller {
     constructor(model, view) {
-        this.model = model
-        this.view = view
+        this.model = model;
+        this.view = view;
     }
 }
 
